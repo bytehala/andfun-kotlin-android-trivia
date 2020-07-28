f8a412aef9f6eac2aa540aedb7e4fde48d66884b
Lem Dulfo <lemuel.dulfo@gmail.com>
2020-07-27 18:02:11 -0600

    Adding Navigation

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -15,15 +17,24 @@ import com.example.android.navigation.databinding.FragmentTitleBinding
  * create an instance of this fragment.
  */
 class TitleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
    
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+
+        // The two ways to lambda an anonymous function. 1st is without KTX, 2nd is with KTX
+//        binding.playButton.setOnClickListener {
+////            view -> Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment) // the old way
+////            view -> view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment) // The KTX way
+//        }
+
+        // Another way is to use the OnClickListener object created by Navigation class
+        // Notice that we're using parentheses, not braces since the 1st and 2nd way both use anonymous functions
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
 
